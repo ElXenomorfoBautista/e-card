c5c00cefc263c2eb02c7e3c9c24eed9947a6c5bf
mariobustosjmz <mario.bustos@phs.mx>
2025-05-22 22:35:00 -0600

    update repository to fix modifiedOn

--- src/repositories/products.repository.ts
+++ src/repositories/products.repository.ts
@@ -10,5 +10,13 @@ export class ProductsRepository extends DefaultCrudRepository<
 > {
     constructor(@inject('datasources.pgdb') dataSource: PgdbDataSource) {
         super(Products, dataSource);
+        this.modelClass.observe('before save', async (context) => {
+            if (context.instance) {
+                context.instance.modifiedOn = new Date();
+            }
+            if (context.data) {
+                context.data.modifiedOn = new Date();
+            }
+        });
     }
 }
